71ad12936eb3a898889eb46316fc26786442adf4
LautiRad <lautaroradboone@gmail.com>
2022-10-31 23:22:14 -0300

    🚀

--- components/Footer.js
+++ components/Footer.js
@@ -2,10 +2,10 @@ import { AiTwotoneHeart } from "react-icons/ai";
 function Footer() {
 	return (
 		<div className="w-full h-16 bg-gray-50 flex justify-center items-center">
-			<h1 className="text-xl text-blue-900 ml-2">
+			<h1 className="text-xl text-blue-400 ml-2">
 				<a
 					href="#"
-					className="cursor-pointer font-semibold text-indigo-900 hover:text-black"
+					className="cursor-pointer font-semibold text-blue-400 hover:text-black"
 				>
 					CreX
 				</a>
