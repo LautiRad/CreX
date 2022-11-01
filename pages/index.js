58a96b6ef35492fb2a7134e71ef7dd313e1061f2
LautiRad <lautaroradboone@gmail.com>
2022-11-01 13:09:05 -0300

    🚀

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import Image from "next/image";
-import HomePage from "./homePage";
-import Navbar from "../components/Navbar";
-import Services from "./services";
-import Works from "./works";
-import Clients from "./clients";
-import Contact from "./contact";
-import Footer from "../components/Footer";
+import Head from "next/head"
+import styles from "../styles/Home.module.css"
+import HomePage from "./homePage"
+import Navbar from "../components/Navbar"
+import Services from "./services"
+import Works from "./works"
+import Clients from "./clients"
+import Contact from "./contact"
+import Footer from "../components/Footer"
 
 export default function Home() {
 	return (
